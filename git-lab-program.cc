e2ff8c5e1bd2399b42bd012d00f2189f0008101e
Jayden Knight <jk834424@ohio.edu>
2024-09-09 13:09:33 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jayden Knight
+ *        Date: September 9th
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
